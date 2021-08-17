88254b9abb23a760743a0415313ca91e2b499543
Chizzo Cheese <chizzo84@gmail.com>
2021-08-17 16:01:29 +0200

    Added Documentation and Handle PIN

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
 # airtel-money-expressjs
 Airtel Money Developer API ExpressJS Client
+
+## Getting Started
+After cloning or downloading this repo, run `npm install`.
+
+Then `npm run start` to start the client API.
+
+Visiting `http://localhost:3000/` should display; *Welcome to your Airtel Money API Client!*.
+
+## Exposed Endpoints
+1. Collection Payment
+    POST - `http://localhost:3000/airtel/collection`
+
+    Sample Payload
+    ```
+        {
+            "amount": 1000,
+            "number": 9999999999
+            "reference": "Testing transaction"
+        }
+    ```
+2. Collection Refund
+    POST - `http://localhost:3000/airtel/collection/refund/{airtelMoneyId}`
+3. Collection Payment Txn Enquiry
+    GET - `http://localhost:3000/airtel/collection/txnEnquiry/{transactionId}`
+4. Disbursement Payment
+    POST - `http://localhost:3000/airtel/disbursement`
+
+    Sample Payload
+    ```
+        {
+            "amount": 1000,
+            "number": 9999999999,
+            "reference": "ABCD07026984141",
+            "pin": 1234
+        }
+    ```
+5. Collection Refund
+    POST - `http://localhost:3000/airtel/disbursement/refund/{airtelMoneyId}`
+
+    Sample Payload
+    ```
+        {
+            "pin": 1234
+        }
+    ```
+6. Collection Payment Txn Enquiry
+    GET - `http://localhost:3000/airtel/collection/txnEnquiry/{transactionId}`
--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3000
 // Airtel Money Developer API Credentials
 const client_id = "*****************************"
 const client_secret = "*****************************"
-const collectionCallbackUrl = "https://backend.lupiya.com/airtel-callback"
+const collectionCallbackUrl = "https://localhost/airtel-callback"
 
 // Generate Bearer Token URL: Authorization
 const tokenUrl = "https://openapiuat.airtel.africa/auth/oauth2/token"
@@ -61,7 +61,7 @@ app.post("/airtel/collection", (req, res) => {
         console.log(JSON.stringify(response.data))
         // Generate Unique Reference
         let rightNow = new Date()
-        let lupiyaRefNo = "LPY" + rightNow.getFullYear() + '-' + (rightNow.getMonth() + 1) + '-' + rightNow.getDate() + '-' + rightNow.getSeconds() + '-' + rightNow.getMilliseconds()
+        let referenceNo = "Ref" + rightNow.getFullYear() + '-' + (rightNow.getMonth() + 1) + '-' + rightNow.getDate() + '-' + rightNow.getSeconds() + '-' + rightNow.getMilliseconds()
         // Make Payment Using Bearer Token
         var data = JSON.stringify({
           "reference": req.body.reference,
@@ -74,7 +74,7 @@ app.post("/airtel/collection", (req, res) => {
             "amount": req.body.amount,
             "country": country,
             "currency": currency,
-            "id": lupiyaRefNo
+            "id": referenceNo
           }
         });
 
@@ -217,8 +217,8 @@ app.get("/airtel/collection/txnEnquiry/:transactionId", (req, res) => {
 
 // Airtel Money Disbursement Payment API
 app.post("/airtel/disbursement", (req, res) => {
-  if (!req.body.amount || !req.body.number || !req.body.reference) {
-    res.json({ "message": "Please provide all required inputs (amount, number and reference)" })
+  if (!req.body.amount || !req.body.number || !req.body.reference || !req.body.pin) {
+    res.json({ "message": "Please provide all required inputs (amount, number, reference and pin)" })
   } else {
     var data = JSON.stringify({
       "client_id": client_id,
@@ -241,9 +241,9 @@ app.post("/airtel/disbursement", (req, res) => {
         console.log(JSON.stringify(response.data))
         // Generate Unique Reference
         let rightNow = new Date()
-        let lupiyaRefNo = "LPY" + rightNow.getFullYear() + '-' + (rightNow.getMonth() + 1) + '-' + rightNow.getDate() + '-' + rightNow.getSeconds() + '-' + rightNow.getMilliseconds()
+        let referenceNo = "Ref" + rightNow.getFullYear() + '-' + (rightNow.getMonth() + 1) + '-' + rightNow.getDate() + '-' + rightNow.getSeconds() + '-' + rightNow.getMilliseconds()
         // PIN Encryption
-        var fourDigitPIN = 1234
+        var fourDigitPIN = req.body.pin
         var pubilc_key = `-----BEGIN PUBLIC KEY-----
         MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCkq3XbDI1s8Lu7SpUBP+bqOs/MC6PKWz
         6n/0UkqTiOZqKqaoZClI3BUDTrSIJsrN1Qx7ivBzsaAYfsB0CygSSWay4iyUcnMVEDrNVO
@@ -265,7 +265,7 @@ app.post("/airtel/disbursement", (req, res) => {
           "pin": encryptedPIN,
           "transaction": {
             "amount": req.body.amount,
-            "id": lupiyaRefNo
+            "id": referenceNo
           }
         });
 
@@ -301,74 +301,78 @@ app.post("/airtel/disbursement", (req, res) => {
 
 // Airtel Money Disbursement Refund Using Airtel Money ID
 app.post("/airtel/disbursement/refund/:airtelMoneyId", (req, res) => {
-  var data = JSON.stringify({
-    "client_id": client_id,
-    "client_secret": client_secret,
-    "grant_type": "client_credentials"
-  });
+  if (!req.body.pin) {
+    res.json({ "message": "Please provide reuired 4-digit PIN" })
+  } else {
+    var data = JSON.stringify({
+      "client_id": client_id,
+      "client_secret": client_secret,
+      "grant_type": "client_credentials"
+    });
 
-  var config = {
-    method: 'post',
-    url: tokenUrl,
-    headers: {
-      'Content-Type': 'application/json',
-      'Accept': '*/*'
-    },
-    data: data
-  };
+    var config = {
+      method: 'post',
+      url: tokenUrl,
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': '*/*'
+      },
+      data: data
+    };
 
-  axios(config)
-    .then(function (response) {
-      console.log(JSON.stringify(response.data))
-      // PIN Encryption
-      var fourDigitPIN = 1234
-      var pubilc_key = `-----BEGIN PUBLIC KEY-----
+    axios(config)
+      .then(function (response) {
+        console.log(JSON.stringify(response.data))
+        // PIN Encryption
+        var fourDigitPIN = req.body.pin;
+        var pubilc_key = `-----BEGIN PUBLIC KEY-----
       MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCkq3XbDI1s8Lu7SpUBP+bqOs/MC6PKWz
       6n/0UkqTiOZqKqaoZClI3BUDTrSIJsrN1Qx7ivBzsaAYfsB0CygSSWay4iyUcnMVEDrNVO
       JwtWvHxpyWJC5RfKBrweW9b8klFa/CfKRtkK730apy0Kxjg+7fF0tB4O3Ic9Gxuv4pFkbQ
       IDAQAB
       -----END PUBLIC KEY-----`
-      // Encrypt with the public key
-      var encrypt = new JSEncrypt();
-      encrypt.setPublicKey(pubilc_key);
-      var encryptedPIN = encrypt.encrypt(fourDigitPIN)
-      console.log("encryptedPIN", encryptedPIN)
+        // Encrypt with the public key
+        var encrypt = new JSEncrypt();
+        encrypt.setPublicKey(pubilc_key);
+        var encryptedPIN = encrypt.encrypt(fourDigitPIN)
+        console.log("encryptedPIN", encryptedPIN)
 
-      // Disbursement Refund Request Using Bearer Token
-      var data = JSON.stringify({
-        "transaction": {
-          "airtel_money_id": req.params.airtelMoneyId
-        },
-        "pin": encryptedPIN
-      });
-
-      var config = {
-        method: 'post',
-        url: disbursementRefundUrl,
-        headers: {
-          'Content-Type': 'application/json',
-          'Accept': '*/*',
-          'X-Country': country,
-          'X-Currency': currency,
-          'Authorization': `Bearer  ${response.data.access_token}`
-        },
-        data: data
-      };
-
-      axios(config)
-        .then(function (response) {
-          console.log(JSON.stringify(response.data))
-          res.json(response.data)
-        })
-        .catch(function (error) {
-          console.log(error)
-          res.json(error.response.data)
+        // Disbursement Refund Request Using Bearer Token
+        var data = JSON.stringify({
+          "transaction": {
+            "airtel_money_id": req.params.airtelMoneyId
+          },
+          "pin": encryptedPIN
         });
-    })
-    .catch(function (error) {
-      console.log(error)
-      res.json(error.response.data)
-    });
+
+        var config = {
+          method: 'post',
+          url: disbursementRefundUrl,
+          headers: {
+            'Content-Type': 'application/json',
+            'Accept': '*/*',
+            'X-Country': country,
+            'X-Currency': currency,
+            'Authorization': `Bearer  ${response.data.access_token}`
+          },
+          data: data
+        };
+
+        axios(config)
+          .then(function (response) {
+            console.log(JSON.stringify(response.data))
+            res.json(response.data)
+          })
+          .catch(function (error) {
+            console.log(error)
+            res.json(error.response.data)
+          });
+      })
+      .catch(function (error) {
+        console.log(error)
+        res.json(error.response.data)
+      });
+  }
 })
 
 // Airtel Money Disbursement Payment Txn Enquiry Using Transaction ID
